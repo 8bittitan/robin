c4900bcb28d60e05424faad534b37913431df6f8
Paul Jankowski <8BitTitan@gmail.com>
2024-04-09 14:21:14 -0400

    Update lspconfig for better completion, add auto close tags

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,19 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = { "windwp/nvim-ts-autotag" },
     build = ":TSUpdate",
     config = function()
+      require("nvim-ts-autotag").setup()
       local config = require("nvim-treesitter.configs")
       config.setup({
+        auto_tag = {
+          enable = true,
+        },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
       })
-    end
-  }
+    end,
+  },
 }
